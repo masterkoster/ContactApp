846ce9b0f8b44505eafd768bbb581d31b96378bd
masterkoster <crunchyforwatch@Gmail.com>
2025-08-05 20:53:20 -0300

    Created a menu

--- Contact.py
+++ Contact.py
@@ -23,6 +23,41 @@ def add_contact():
 
     else:
         print(' Please type "Yes" or "No": \n')
+def search_contact():
+    
+
+contacts = []
+
+while True:
+    print("\n Contact app menu:")
+    print("1.Add contacts")
+    print("2 Search Contacts")
+    print("3 Delete contacts")
+    print("4 Show contacts")
+    print("5 Exit")
+
+    choice = input("Choose an input (1-5): ")
+
+    if choice == "1":
+        contact = add_contact()
+        contacts.append(contact)
+
+
+    elif choice == "2":
+        print("")
+
+    elif choice == "":
+        print("")
+
+    elif choice == "4":
+        print("")
+
+    elif choice == "5":
+        print("")
+
+
+
+
 
 
 contacts = []
