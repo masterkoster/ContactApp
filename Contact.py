fbd55af86934695cb2b902abb32f4f13906beb29
masterkoster <crunchyforwatch@Gmail.com>
2025-08-07 23:36:06 -0300

    Can make changes to contacts now, although things like spaces for names do not work, and having the otion for a country code and or a requirement of ten numbers check is missing as well

--- Contact.py
+++ Contact.py
@@ -10,63 +10,84 @@ def load_contact():
         with open("contacts.json", "r") as f:
             contacts = json.load(f)
     except FileNotFoundError:
-        contacts
-contacts = []
+        contacts = []
+
+
 load_contact()
 
+
+#Dumps new info in file
 def save_contact():
-    with open ("contacts.json", "w") as f:
+    with open("contacts.json", "w") as f:
         json.dump(contacts, f)
-
-
 
 
 #Add_contact asks for their name and number, a confirmation before creation and if not, makes the user repeat their input
 def add_contact():
- while True:
-    UserName = input("Enter your name: ")
-    UserNumber = input("Enter your Number: ")
+    while True:
+        UserName = input("Enter your name: ")
+        UserNumber = input("Enter your Number: ")
 
-    contactInfo = {
-        "Name": UserName,
-        "Number": UserNumber
-    }
+        contactInfo = {
+            "Name": UserName,
+            "Number": UserNumber
+        }
 
-    if not UserName.isalpha():
-        print("Please use only letters in the name: ")
-        continue
+        if not UserName.isalpha():
+            print("Please use only letters in the name: ")
+            continue
 
-    if not UserNumber.isdigit():
-        print("Please use only numbers for the phone number: ")
-        continue
+        if not UserNumber.isdigit():
+            print("Please use only numbers for the phone number: ")
+            continue
 
+        print(contactInfo)
 
+        # if else statement confirmations
+        userConfirmation = input('Is information correct?, respond with "Yes" or "No: ')
 
-    print(contactInfo)
-
-# if else statement confirmations
-    userConfirmation = input( 'Is information correct?, respond with "Yes" or "No: ')
-
-    if userConfirmation.lower() in ("yes", "y"):
-        print(" Thanks! Information saved")
-        save_contact()
-        return contactInfo
+        if userConfirmation.lower() in ("yes", "y"):
+            print(" Thanks! Information saved")
+            save_contact()
+            return
 
 
-    elif userConfirmation.lower() in ("no", "n"):
-        print(" Let's try again . \n")
+        elif userConfirmation.lower() in ("no", "n"):
+            print(" Let's try again . \n")
 
 
-    else:
-        print(' Please type "Yes" or "No": \n')
+        else:
+            print(' Please type "Yes" or "No": \n')
 
 
+def editContact():
+    name = input("Enter the name of the contact you want to edit:")
+    for c in contacts:
+        if c["Name"].lower() == name.lower():
+           print(f"{name} has been found")
+           newName = input("Enter their new name")
+           newNumber = input("Enter their new number")
 
+           if newName:
+               if not newName.isalpha():
+                  print("Please use only letters in the name: ")
+                  return
+               c["Name"] = newName
 
+           if newNumber:
+              if not newNumber.isdigit():
+                print("Please use only letters in the name: ")
+                return
+              c["Number"] = newNumber
 
+           save_contact()
+           print("✅ Contact updated successfully.")
+           return  # stop after editing
+
+    print("❌ Contact not found.")
 
 #delete function that asks for an input an then looks through the contact list, then proceeds to delete thn and save it in the json file
-def delete_contact():
+def deleteContact():
     name = input("Enter the name of the contact  you want to delete: ")
     for c in contacts:
         if c["Name"].lower() == name.lower():
@@ -75,7 +96,10 @@ def delete_contact():
             save_contact()
             return
     print("Contact not found.")
-save_contact()
+
+
+
+
 #Searches for the name saved in input
 def showContact():
     if not contacts:
@@ -83,6 +107,7 @@ def showContact():
     else:
         for contact in contacts:
             print(f"{contact['Name']} {contact['Number']}")
+
 
 #contact works as the "database" although it has no real save function after restarting the program
 
@@ -105,16 +130,18 @@ def searchContact():
         else:
             break  # found the contact, no need to search again
 
+
 #Menu inputs
 while True:
     print("\n Contact app menu:")
-    print("1 Add contacts")
-    print("2 Search Contacts")
-    print("3 Delete contacts")
-    print("4 Show contacts")
-    print("5 Exit")
+    print("1: Add contacts")
+    print("2: Search Contacts")
+    print("3: Edit Contacts")
+    print("4: Delete contacts")
+    print("5: Show contacts")
+    print("6: Exit")
 
-# The options after asking for an input which will lead to a function being executed. Although I suppose I could put it in a def and keep it cleaner..
+    # The options after asking for an input which will lead to a function being executed. Although I suppose I could put it in a def and keep it cleaner..
     choice = input("Choose an input (1-5): ")
 
     if choice == "1":
@@ -122,25 +149,20 @@ while True:
         contacts.append(contact)
         save_contact()
 
-
     elif choice == "2":
         searchContact()
 
-
-
     elif choice == "3":
-        delete_contact()
-        save_contact()
-
-
+        editContact()
 
     elif choice == "4":
-        print(showContact())
-
+        deleteContact()
 
     elif choice == "5":
+        showContact()
+
+    elif choice == "6":
         break
 
     else:
-        break
-
+        print("Try again")
